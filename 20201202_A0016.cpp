edc383caa16b22d277148f0048a2fabf06f81487
Yunho Kim <hrs350x@gmail.com>
2021-02-05 09:47:25 +0900

    Create 20201202_A0016.cpp

---
+++ 20201202_A0016.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+vector< pair<int, float> > map[201];
+
+int main() {
+	int CA;
+	ifstream f("input.txt");
+	cin >> CA;
+
+	for (int ca = 1; ca <= CA; ca++) {
+		int V, E, T;
+		cin >> V >> E >> T;
+		//initialize
+		for (int i = 1; i <= V; i++) {
+			map[i].clear();
+		}
+
+		for (int i = 0; i < E; i++) {
+			int a, b;
+			float p;
+			cin >> a >> b >> p;
+			map[a].push_back(make_pair(b, p));
+		}
+
+		vector<float> now;
+		vector<float> pre;
+
+		//initialize
+		pre.push_back(0);
+		pre.push_back(1);
+		now.push_back(0);
+		now.push_back(0);
+		for (int i = 2; i <= V; i++) {
+			pre.push_back(0);
+			now.push_back(0);
+		}
+
+		float res = 0;
+		int res_state = 0;
+
+		for (int t = 1; t <= T / 10 + 1; t++) {
+			for (int i = 1; i <= V; i++) {
+				for (vector<pair<int, float> >::iterator it = map[i].begin(); it != map[i].end(); ++it) {
+					now[it->first] += pre[i] * it->second;
+				}	
+			}
+			res = 0;
+			for (int j = 1; j <= V; j++) {
+				if (res < pre[j]) {
+					res = max(res, pre[j]);
+					res_state = j;
+				}
+			}
+
+			pre = now;
+			for (int i = 1; i <= V; i++) {
+				now[i] = 0;
+			}	
+		}	
+
+		if (res < 0.0000001) {
+			printf("#%d 0\n", ca);
+
+		}
+		else {
+			printf("#%d %d %.6f\n", ca, res_state, res);
+		}
+	}
+}
