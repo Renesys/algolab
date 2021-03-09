54e1d48e257db16c86bea1f38d23c4d28f4f82a2
Yunho Kim <hrs350x@gmail.com>
2021-03-09 22:29:43 +0900

    Update 20210309_PE0020.cpp

--- 20210309_PE0020.cpp
+++ 20210309_PE0020.cpp
@@ -124,4 +124,86 @@ int main() {
 
 
 
+==========================================
+	
+	
+	
+#include<iostream>
+#include<fstream>
+#include<cstdio>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+struct Runner {
+	int idx;
+	int data;
+};
+
+Runner r[100001];
+Runner tmp[100001];
+int res = 0;
+
+void merge(int s, int m, int e) {
+	int f_idx = s;
+	int r_idx = m + 1;
+	int t = 0;
+
+	while (f_idx <= m || r_idx <= e) {
+		if (f_idx > m) {
+			tmp[t++] = r[r_idx++];
+		}
+		else if (r_idx > e) {
+			tmp[t++] = r[f_idx++];
+		}
+		else {
+			if (r[r_idx].data > r[f_idx].data) {
+				tmp[t++] = r[r_idx++];
+				res += (m - f_idx + 1);
+			}
+			else {
+				tmp[t++] = r[f_idx++];
+			}
+		}
+	}
+
+	for (int i = 0; i < t; i++) {
+		r[i + s] = tmp[i];
+	}
+}
+
+void mergesort(int s, int e) {
+	if (s == e) {
+		return;
+	}
+
+	int m = (s + e) / 2;
+
+	mergesort(s, m);
+	mergesort(m + 1, e);
+	merge(s, m, e);
+}
+
+int main() {
+	ifstream f("input.txt");
+	int CA;
+	f >> CA;
+	for (int ca = 1; ca <= CA; ca++) {
+		int N;
+		f >> N;
+		res = 0;
+		for (int i = 1; i <= N; i++) {
+			int a;
+			f >> a;
+			r[i] = { 0, a };
+		}
+
+		mergesort(1, N);
+
+		printf("#%d %d\n", ca, res);
+
+	}
+	return 0;
+}
+
 
