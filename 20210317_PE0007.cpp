76209479a178bc3069049cfa036106b0bfca01b0
Yunho Kim <hrs350x@gmail.com>
2021-03-17 16:36:37 +0900

    Update 20210317_PE0007.cpp

--- 20210317_PE0007.cpp
+++ 20210317_PE0007.cpp
@@ -1,3 +1,54 @@
+#include<iostream>
+#include<fstream>
+#include<cstdio>
+#include<algorithm>
+#define MAX 1000000000
+using namespace std;
+
+long long dist[301][301];
+
+int main() {
+	ifstream f("input.txt");
+	int CA;
+	f >> CA;
+
+	for (int ca = 1; ca <= CA; ca++) {
+		int V;
+		f >> V;
+
+		for (int i = 1; i <= V; i++) {
+			for (int j = 1; j <= V; j++) {
+				f >> dist[i][j];
+			}
+		}
+
+		long long res = 0;
+
+		for (int k = 1; k <= V; k++) {
+			for (int i = 1; i <= V; i++) {
+				for (int j = 1; j <= V; j++) {
+					dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);	
+				}
+			}
+		}
+
+		for (int i = 1; i <= V; i++) {
+			for (int j = 1; j <= V; j++) {
+				if (dist[i][j] > res && dist[i][j] != MAX && dist[i][j] != 0) {
+					res = dist[i][j];
+				}
+			}
+		}
+
+		printf("#%d %lld\n", ca, res);
+
+
+	}
+	return 0;
+}
+
+===============================
+
 2
 4
 0 5 3 1
