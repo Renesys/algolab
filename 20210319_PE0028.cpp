7d6f0df038b3145a7c4089b4ad2d001c848f2b14
Yunho Kim <hrs350x@gmail.com>
2021-03-18 14:49:43 +0900

    Create 20210319_PE0028.cpp

---
+++ 20210319_PE0028.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<fstream>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<algorithm>
+#define MAX 1000000000
+using namespace std;
+
+
+int D[1001][1001];
+
+int main() {
+	ifstream f("input.txt");
+	int CA;
+	f >> CA;
+
+	for (int ca = 1; ca <= CA; ca++) {
+		string A = "0", B = "0";
+		string as, bs;
+		f >> as >> bs;
+		A += as;
+		B += bs;
+
+		for (int i = 0; i <= A.size(); i++) {
+			for (int j = 0; j <= B.size(); j++) {
+				D[i][j] = 0;
+			}
+		}
+
+		for (int i = 1; i < B.size(); i++) {
+			for (int j = 1; j < A.size(); j++) {
+				if (A[j] == B[i]) {
+					D[i][j] = D[i - 1][j - 1] + 1;
+				}
+				else {
+					D[i][j] = max(D[i - 1][j], D[i][j - 1]);
+				}
+			}
+		}
+
+		//search
+		vector<char> res;
+		int a = A.size() - 1;
+		int b = B.size() - 1;
+		while (a != 0 && b != 0) {
+			if (D[b][a] != D[b][a - 1]) {
+				res.push_back(A[a]);
+				b--;
+			}
+			a--;
+		}
+
+		printf("#%d ", ca);
+		while (!res.empty()) {
+			printf("%c", res.back());
+			res.pop_back();
+		}
+		printf("\n");
+	}
+	return 0;
+}
